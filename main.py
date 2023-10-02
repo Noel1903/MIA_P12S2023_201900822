ad7b74c57018dd3459d0e1cbc98f6d1f64e90c31
Noel1903 <osmar19lemus@gmail.com>
2023-10-02 11:21:52 -0600

    Inicializacion de frontend para el proyecto 2

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from  grammar.grammar import parser
-print("Ingrese el comando path para leer el archivo")
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def initial():
+    return "Welcome to the server"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0",port=4000)
+'''print("Ingrese el comando path para leer el archivo")
 line = input()
 #execute -path=/home/usuario/example.txt
 command = line.split("=")
@@ -7,4 +18,5 @@ path = command[1]
 
 with open(path, 'r') as file:
     data = file.read()
-    parser.parse(data)
+    parser.parse(data)'''
+
